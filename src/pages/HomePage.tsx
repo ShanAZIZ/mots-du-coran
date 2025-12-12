5d0a0b6877052cc6512bc0e6af3fa160ca7af232
Shan AZIZ <shan.azizh@gmail.com>
2025-12-12 22:52:11 +0100

    feat: homepage rework

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,18 +2,25 @@ import { Link } from "react-router";
 
 export function HomePage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center px-4">
-      <h1 className="text-3xl md:text-5xl font-bold text-center text-primary-content mb-10">
-        Apprenez 80% des mots du Qur&apos;an
-      </h1>
-      <div className="flex flex-col md:flex-row gap-4">
-        <Link to="/mots" className="btn btn-primary btn-xl w-48 text-center">
-          Mots
-        </Link>
-        <Link to="/quizz" className="btn btn-secondary btn-xl w-48 text-center">
-          Quizz !
-        </Link>
+    <div className="min-h-screen bg-base-200 flex flex-col justify-center items-center p-6">
+      <div className="text-center max-w-lg w-full">
+        <h1 className="text-4xl font-bold mb-4">
+          Révise le Vocabulaire du Coran
+        </h1>
+        <p className="text-lg opacity-80 mb-8">
+          Accède rapidement aux mots essentiels et entraîne-toi avec des quiz
+          adaptés.
+        </p>
+
+        <div className="flex flex-col gap-4 w-full">
+          <Link to="/mots" className="btn btn-primary w-full text-lg">
+            📘 Accéder aux mots
+          </Link>
+          <Link to="/quizz" className="btn btn-secondary w-full text-lg">
+            📝 Faire un quiz
+          </Link>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
